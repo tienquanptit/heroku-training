de543d12f36931e2a93800122a2e7719c3e10a7d
QuanTien <vinhnt9x@gmail.com>
2020-08-10 14:01:11 +0700

    Config database heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL = parse_url('postgres://rljqkuxfauwutp:e3a13e281fa4834fa378a1f045afcd7ec5ed16f45435fefc4501fe4f1eb8e7d5@ec2-18-211-48-247.compute-1.amazonaws.com:5432/d5kdru2pk47ejd');
+
 return [
 
     /*
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'psql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -68,7 +70,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL['path'], '/'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
